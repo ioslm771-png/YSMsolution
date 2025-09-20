224732359989292e4fa1edab79121c838385d24b
yeslem <ioslm771@gmail.com>
2025-09-20 22:53:13 +0300

    Update Form1.vb

--- YSMproject/Form1.vb
+++ YSMproject/Form1.vb
@@ -22,4 +22,13 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
 End Class
